364543f35769444f0c4d7053caa2fed2ba2d9082
Ekaterina Vorozheina <evorozheina@ozon.ru>
2024-01-20 16:42:00 +0300

    Домашняя работа перевыложена, т.к. был ошибочно выложен черновик. Приложены файлы:  - FF_project_description.pdf - описание БД  - FF_project_diagram.pdf - диаграмма БД со связями  - FF_project_script.sql - сформированный скрипт SQL

--- HW12/FF_project_script.sql
+++ HW12/FF_project_script.sql
@@ -2,16 +2,7 @@ CREATE TABLE [Item] (
   [ID] bigint PRIMARY KEY,
   [Name] varchar(max),
   [Descript] varchar(max),
-  [StateID] bigint,
   [ItemType] varchar(max)
-)
-GO
-
-CREATE TABLE [State] (
-  [ID] bigint PRIMARY KEY,
-  [Name] varchar(max),
-  [Descript] varchar(max),
-  [SysName] varchar(max)
 )
 GO
 
@@ -20,7 +11,6 @@ CREATE TABLE [Place] (
   [Barcode] varchar(50),
   [ContractID] bigint,
   [OwnerObjectID] bigint,
-  [StateID] bigint,
   [Name] varchar(max),
   [Descript] varchar(max),
   [PlaceType] varchar(max)
@@ -31,13 +21,12 @@ CREATE TABLE [Contract] (
   [ID] bigint PRIMARY KEY,
   [Date] datetime,
   [Num] varchar(max),
-  [CustomerID] bigint,
-  [PerformerID] bigint,
+  [CustomerID] bigint NOT NULL,
+  [PerformerID] bigint NOT NULL,
   [StopDate] datetime,
   [NDSFree] tinyint,
   [Name] varchar(max),
   [Descript] varchar(max),
-  [StateID] bigint,
   [ContractType] varchar(max)
 )
 GO
@@ -50,7 +39,6 @@ CREATE TABLE [Contractor] (
   [LegalAddressStr] varchar(max),
   [ShortName] varchar(255),
   [Descript] varchar(max),
-  [StateID] bigint,
   [ContractorType] varchar(max)
 )
 GO
@@ -59,9 +47,8 @@ CREATE TABLE [Route] (
   [ID] bigint PRIMARY KEY,
   [Name] varchar(max),
   [Descript] varchar(max),
-  [MinPlaceID] bigint,
-  [MaxPlaceID] bigint,
-  [StateID] bigint
+  [MinPlaceID] bigint NOT NULL,
+  [MaxPlaceID] bigint NOT NULL
 )
 GO
 
@@ -76,47 +63,44 @@ CREATE TABLE [Carriage] (
   [Trucker] varchar(max),
   [SendDate] datetime,
   [ArriveDate] datetime,
-  [StateID] bigint
+  [CarriageState] nvarchar(255) NOT NULL CHECK ([CarriageState] IN ('Banded', 'Sent', 'Arrived', 'Closed'))
 )
 GO
 
 CREATE TABLE [Documents] (
   [ID] bigint PRIMARY KEY,
-  [Num] varchar(max),
-  [Name] varchar(max),
+  [DateIn] datetime NOT NULL,
+  [IntNum] varchar(max) NOT NULL,
   [Amount] money,
   [SenderPersonID] bigint,
   [ReceiverPersonID] bigint,
-  [DateIn] datetime,
-  [IntNum] varchar(max),
   [ContractID] bigint,
   [CarriageID] bigint,
+  [Name] varchar(max),
   [Descript] varchar(max),
-  [StateID] bigint,
   [DocumentType] varchar(max)
 )
 GO
 
 CREATE TABLE [Article] (
   [ID] bigint PRIMARY KEY,
-  [ItemID] bigint,
+  [ItemID] bigint NOT NULL,
   [PlaceID] bigint,
   [DstPlaceID] bigint,
   [Price] money,
   [Cost] money,
-  [ContractorID] bigint,
+  [ContractorID] bigint NOT NULL,
   [ContainerID] bigint,
   [RouteID] bigint,
   [Barcode] varchar(50),
   [Weight] bigint,
   [DeliveryPrice] money,
   [IsReturn] tinyint,
-  [ContractID] bigint,
-  [StateID] bigint
+  [ContractID] bigint
 )
 GO
 
-CREATE TABLE [HumanPerson] (
+CREATE TABLE [PersonHuman] (
   [ID] bigint PRIMARY KEY,
   [LastName] varchar(max),
   [FirstName] varchar(max),
@@ -139,7 +123,6 @@ GO
 CREATE TABLE [ArticleInCarriage] (
   [ArticleID] bigint NOT NULL,
   [CarriageID] bigint NOT NULL,
-  [PlaceID] bigint,
   [MomentIn] datetime NOT NULL,
   [MomentOut] datetime,
   [TakeOffPlaceID] bigint,
@@ -164,7 +147,7 @@ CREATE TABLE [ArticleInDocument] (
   [Price] money,
   [Cost] money,
   [NDS] float,
-  [DocumentID] bigint NOT NULL,
+  [DocumentID] bigint,
   [ObjectType] varchar(max),
   [NDSValue] money,
   [Name] varchar(max),
@@ -172,13 +155,10 @@ CREATE TABLE [ArticleInDocument] (
 )
 GO
 
-ALTER TABLE [Item] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
+ALTER TABLE [Place] ADD FOREIGN KEY ([ContractID]) REFERENCES [Contract] ([ID])
 GO
 
 ALTER TABLE [Place] ADD FOREIGN KEY ([OwnerObjectID]) REFERENCES [Place] ([ID])
-GO
-
-ALTER TABLE [Place] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
 GO
 
 ALTER TABLE [Contract] ADD FOREIGN KEY ([CustomerID]) REFERENCES [Contractor] ([ID])
@@ -187,40 +167,25 @@ GO
 ALTER TABLE [Contract] ADD FOREIGN KEY ([PerformerID]) REFERENCES [Contractor] ([ID])
 GO
 
-ALTER TABLE [Contract] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
-GO
-
-ALTER TABLE [Contractor] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
-GO
-
 ALTER TABLE [Route] ADD FOREIGN KEY ([MinPlaceID]) REFERENCES [Place] ([ID])
 GO
 
 ALTER TABLE [Route] ADD FOREIGN KEY ([MaxPlaceID]) REFERENCES [Place] ([ID])
 GO
 
-ALTER TABLE [Route] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
-GO
-
 ALTER TABLE [Carriage] ADD FOREIGN KEY ([RouteID]) REFERENCES [Route] ([ID])
 GO
 
-ALTER TABLE [Carriage] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
+ALTER TABLE [Documents] ADD FOREIGN KEY ([SenderPersonID]) REFERENCES [PersonHuman] ([ID])
 GO
 
-ALTER TABLE [Documents] ADD FOREIGN KEY ([SenderPersonID]) REFERENCES [HumanPerson] ([ID])
-GO
-
-ALTER TABLE [Documents] ADD FOREIGN KEY ([ReceiverPersonID]) REFERENCES [HumanPerson] ([ID])
+ALTER TABLE [Documents] ADD FOREIGN KEY ([ReceiverPersonID]) REFERENCES [PersonHuman] ([ID])
 GO
 
 ALTER TABLE [Documents] ADD FOREIGN KEY ([ContractID]) REFERENCES [Contract] ([ID])
 GO
 
 ALTER TABLE [Documents] ADD FOREIGN KEY ([CarriageID]) REFERENCES [Carriage] ([ID])
-GO
-
-ALTER TABLE [Documents] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
 GO
 
 ALTER TABLE [Article] ADD FOREIGN KEY ([ItemID]) REFERENCES [Item] ([ID])
@@ -244,9 +209,6 @@ GO
 ALTER TABLE [Article] ADD FOREIGN KEY ([ContractID]) REFERENCES [Contract] ([ID])
 GO
 
-ALTER TABLE [Article] ADD FOREIGN KEY ([StateID]) REFERENCES [State] ([ID])
-GO
-
 ALTER TABLE [CarriagePlace] ADD FOREIGN KEY ([CarriageID]) REFERENCES [Carriage] ([ID])
 GO
 
@@ -257,9 +219,6 @@ ALTER TABLE [ArticleInCarriage] ADD FOREIGN KEY ([ArticleID]) REFERENCES [Articl
 GO
 
 ALTER TABLE [ArticleInCarriage] ADD FOREIGN KEY ([CarriageID]) REFERENCES [Carriage] ([ID])
-GO
-
-ALTER TABLE [ArticleInCarriage] ADD FOREIGN KEY ([PlaceID]) REFERENCES [Place] ([ID])
 GO
 
 ALTER TABLE [ArticleInCarriage] ADD FOREIGN KEY ([TakeOffPlaceID]) REFERENCES [Place] ([ID])
